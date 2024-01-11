48d209c5e8fff349bd970cabf3e29f16ffcd2777
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2024-01-11 17:51:33 +0900

    完成

---
+++ DirectXGame/Scene.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+enum class SceneType {
+	kTitle,
+	kGamePlay,
+	kGameClear,
+};
---
+++ DirectXGame/Title.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+#include "Scene.h"
+
+class Title {
+public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	Title();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~Title();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	void RoopInitialize();
+
+	bool IsSceneEnd() { return isSceneEnd; }
+	SceneType NextScene(){return SceneType::kGamePlay;}
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
+	uint32_t titleTexture = 0;
+	Sprite* titleSprite_ = nullptr;
+
+	bool isSceneEnd = false;
+};
